6d9ecf717c8b8db85cfc69ab3558903979757688
Devon Blandin <dblandin@gmail.com>
2015-08-20 22:11:56 +0200

    Add spec verifying no issue for methods lacking complexity

--- spec/cc/engine/mcclimate_spec.rb
+++ spec/cc/engine/mcclimate_spec.rb
@@ -6,30 +6,8 @@ module CC::Engine
     include EngineHelper
 
     describe '#run' do
-      it 'honors the exclude_paths configuration option' do
-        create_source_file('foo.rb', <<-RUBY)
-          def foo
-            x = 1 + 3
-            y = 2 * 4 + 6
-            z = x * y / 2
-          end
-        RUBY
-
-        config = { exclude_paths: ['foo.rb'] }
-
-        Mcclimate.new(code_path: code_path, config: config, output_io: output_io).run
-
-        expect(issues).to be_empty
-      end
-
-      it 'analyzes source ruby files for complexity' do
-        create_source_file('foo.rb', <<-RUBY)
-          def foo
-            x = 1 + 3
-            y = 2 * 4 + 6
-            z = x * y / 2
-          end
-        RUBY
+      it 'analyzes source ruby files for method complexity' do
+        create_source_file('foo.rb', complex_method_body)
 
         Mcclimate.new(code_path: code_path, output_io: output_io).run
 
@@ -45,6 +23,46 @@ module CC::Engine
           }
         )
       end
+
+      it 'should not report issues for methods with a complexity score <= 10' do
+        create_source_file('foo.rb', not_complex_method_body)
+
+        Mcclimate.new(code_path: code_path, output_io: output_io).run
+
+        expect(issues).to be_empty
+      end
+
+      it 'honors the exclude_paths configuration option' do
+        create_source_file('foo.rb', complex_method_body)
+
+        config = { exclude_paths: ['foo.rb'] }
+
+        Mcclimate.new(code_path: code_path, config: config, output_io: output_io).run
+
+        expect(issues).to be_empty
+      end
+    end
+
+    private
+
+    def complex_method_body
+      # complexity score of 12 (1 + 5 operands + 6 integers)
+      <<-RUBY
+        def foo
+          x = 1 + 3
+          y = 2 * 4 + 6
+          z = x * y / 2
+        end
+      RUBY
+    end
+
+    def not_complex_method_body
+      # complexity score of 4 (1 + 1 operand + 2 integers)
+      <<-RUBY
+        def foo
+          x = 1 + 3
+        end
+      RUBY
     end
   end
 end
